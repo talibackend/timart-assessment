2fa50a5a6eb915de9bbcc51a710ff31dd2a8a202
Femi Fatokun <76601789+talibackend@users.noreply.github.com>
2023-10-20 00:14:45 +0100

    Added basic user type

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const { database } = require('./config/database');
 require('./models/index');
+const { graphqlHTTP } = require('express-graphql');
 
 database.sync({ alter : true }).then(()=>{
     app.listen(process.env.PORT, ()=>{ console.log(`Server running on :${process.env.PORT}`) });
 }).catch(error=>{ console.error(`Database connection failed.\nError: ${error}`) });
+
+app.use('/graphql', graphqlHTTP({ graphiql : true }))
 
 module.exports = app;
