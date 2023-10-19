3cebdedced869ba4cd08fca69abee795b3e03807
Femi Fatokun <76601789+talibackend@users.noreply.github.com>
2023-10-19 23:40:47 +0100

    Made some updates

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const { database } = require('./config/database');
 
-app.listen(process.env.PORT, ()=>{ console.log(`Server running on :${process.env.PORT}`) });
+database.sync({ alter : true }).then(()=>{
+    app.listen(process.env.PORT, ()=>{ console.log(`Server running on :${process.env.PORT}`) });
+}).catch(error=>{ console.error(`Database connection failed.\nError: ${error}`) });
 
 module.exports = app;
