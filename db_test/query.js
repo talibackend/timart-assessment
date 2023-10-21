9939a83780861c4ea1bec90675bb0aa277c8b69b
Femi Fatokun <76601789+talibackend@users.noreply.github.com>
2023-10-21 23:35:51 +0100

    We are finally done here

---
+++ db_test/query.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: Number(process.env.DB_PORT)
+});
+
+connection.query(`SELECT 
+                    u.id, 
+                    u.username, 
+                    u.first_name, 
+                    u.last_name, 
+                    u.email, 
+                    COUNT(o.id) AS order_count
+                    FROM Users AS u
+                    JOIN Orders AS o ON u.id = o.UserId
+                    GROUP BY u.id, u.username, u.first_name, u.last_name, u.email
+                    ORDER BY order_count DESC
+                    LIMIT 10`, (err, result) => {
+    if (err) {
+        console.log(err);
+        throw new Error("Sorry an error occured");
+    }
+    console.log(result);
+    connection.end();
+})
